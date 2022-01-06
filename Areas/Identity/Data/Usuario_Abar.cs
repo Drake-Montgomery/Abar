3a8f62fdf66e3b4c4e475f1c0a1fed3eadfe6a5c
Drake-Montgomery <eder_vip@outlook.com>
2022-01-06 10:26:42 -0300

    HTML login page updatedAdd project files.

---
+++ Areas/Identity/Data/Usuario_Abar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Abar.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Usuario_Abar class
+public class Usuario_Abar : IdentityUser
+{
+    [Required]
+    [PersonalData]
+    [Column(TypeName = "nvarchar(50)")]
+    public string? Nome { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(50)")]
+    public string? Sobrenome { get; set; }
+}
+
